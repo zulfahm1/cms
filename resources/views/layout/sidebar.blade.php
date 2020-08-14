63eca024ce11390a8d567784694b2a2bf09ef8b3
zulfahm1 <zoelfahmisyam03@gmail.com>
2020-08-14 21:51:45 +0700

    skuy

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -5,7 +5,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">CMS Admin <sup>F</sup></div>
+        <div class="sidebar-brand-text mx-3">Admin Skuy <sup>F</sup></div>
       </a>
 
       <!-- Divider -->
